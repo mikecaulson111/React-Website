8bc24cd742c3e301039702ef7d0c230ac7d88c7b
michael.caulson <mikecaulson111@gmail.com>
2025-09-06 22:11:04 -0600

    Adding the next piece

--- First-React-App/src/components/PayoffMinimum.jsx
+++ First-React-App/src/components/PayoffMinimum.jsx
@@ -1,10 +1,67 @@
 import { useState } from "react"
 
 export default function PayoffMinimum() {
+    // These are to be entered by the user
+    const [loanAmt, setLoanAmt] = useState("");
+    const [interest, setInterest] = useState("");
+    const [months, setMonths] = useState("");
+    // To be calculated
+    const [payoffAmt, setPayoffAmt] = useState(0);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let doubleInterest = parseFloat(interest) / 100.0 / 12.0;
+        let pow = Math.pow(1 + doubleInterest, parseInt(months));
+        setPayoffAmt(loanAmt * (doubleInterest * pow) / (pow - 1));
+
+    }
+
+    const handleLoanAmtChange = (e) => {
+        setLoanAmt(e.target.value);
+    }
+
+    const handleInterestChange = (e) => {
+        setInterest(e.target.value);
+    }
+
+    const handleMonthsChange = (e) => {
+        setMonths(e.target.value);
+    }
 
     return (
         <>
             <h3>This is for calculating minimum needed to pay off loan in x months</h3>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Loan Amount $
+                    <input
+                        type="number"
+                        value={loanAmt}
+                        onChange={handleLoanAmtChange}
+                    />
+                </label>
+                <label>
+                    Interest on loan %
+                    <input
+                        type="number"
+                        value={interest}
+                        onChange={handleInterestChange}
+                    />
+                </label>
+                <label>
+                    Number of months loan is for #
+                    <input
+                        type="number"
+                        value={months}
+                        onChange={handleMonthsChange}
+                    />
+                </label>
+                <button type="submit">Calculate</button>
+            </form>
+
+            <h3>Minimum monthly payments to pay off ${loanAmt} at {interest}% in {months} months is: ${payoffAmt.toFixed(2)}</h3>
         </>
     )
 }
