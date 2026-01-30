b099f54f7b2f492a9639800ecd911938e31ec7c2
Michael Caulson <mikecaulson111@gmail.com>
2026-01-30 13:46:49 -0700

    fixing building error

--- First-React-App/src/components/GitHubModule/GitHubModule.jsx
+++ First-React-App/src/components/GitHubModule/GitHubModule.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./GitHubModule.css";
+import "./GithubModule.css";
 
 const filterLastWeek = (events, dayOfWeek) => {
     const lastWeek = new Date();
