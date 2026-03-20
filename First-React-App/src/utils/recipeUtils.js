2cf3fd9f47815adc28b34d134cd38f87221bee15
Michael Caulson <mikecaulson111@gmail.com>
2026-03-20 14:05:15 -0600

    getting ready for deployment

--- First-React-App/src/utils/recipeUtils.js
+++ First-React-App/src/utils/recipeUtils.js
@@ -1,29 +1,29 @@
 // This should hopefully eventually be replaced with calls to supabase!
 export const recipes = [
-  {
-    id: 1,
-    title: "Spicy Garlic Shrimp",
-    time: "20 mins",
-    difficulty: "Easy",
-    image: "https://therecipecritic.com/wp-content/uploads/2016/10/brownbuttergarlicshrimp3.jpg",
-    description: "Brown Butter Spicy Garlic Shrimp is an incredibly easy meal that is ready in under 10 minutes! The brown butter flavor is so luxurious over the spicy garlic shrimp. It will wow your family!",
-    ingredients: [
-        "4 tablespoons butter",
-        "1 lb large peeled and devined shrimp",
-        "salt and pepper",
-        "3 cloves minced garlic",
-        "juice from 1 lemon",
-        "1/4 teaspoon red pepper flakes",
-        "2 tablespoons chopped parsley"
-    ],
-    instructions: [
-        "In a medium sized skillet over medium low heat add 4 tablespoons unsalted butter. Cook until butter starts to foam, and turn brown. About 3-4 minutes.",
-        "Add 1 pound large peeled and deveined shrimp and season with salt and pepper. Turn the heat to medium high. Add 3 cloves minced garlic. Cook until the shrimp turns pink about 2 minutes each side.",
-        "Add juice of one lemon, and ¼ teaspoon red pepper flakes. Garnish with 2 tablespoons chopped parsley and toss with pasta if desired. Serve immediately .",
-    ],
-    link: "https://therecipecritic.com/brown-butter-spicy-garlic-shrimp/",
-    slug: "spicy-garlic-shrimp"
-  },
+  // {
+  //   id: 1,
+  //   title: "Spicy Garlic Shrimp",
+  //   time: "20 mins",
+  //   difficulty: "Easy",
+  //   image: "https://therecipecritic.com/wp-content/uploads/2016/10/brownbuttergarlicshrimp3.jpg",
+  //   description: "Brown Butter Spicy Garlic Shrimp is an incredibly easy meal that is ready in under 10 minutes! The brown butter flavor is so luxurious over the spicy garlic shrimp. It will wow your family!",
+  //   ingredients: [
+  //       "4 tablespoons butter",
+  //       "1 lb large peeled and devined shrimp",
+  //       "salt and pepper",
+  //       "3 cloves minced garlic",
+  //       "juice from 1 lemon",
+  //       "1/4 teaspoon red pepper flakes",
+  //       "2 tablespoons chopped parsley"
+  //   ],
+  //   instructions: [
+  //       "In a medium sized skillet over medium low heat add 4 tablespoons unsalted butter. Cook until butter starts to foam, and turn brown. About 3-4 minutes.",
+  //       "Add 1 pound large peeled and deveined shrimp and season with salt and pepper. Turn the heat to medium high. Add 3 cloves minced garlic. Cook until the shrimp turns pink about 2 minutes each side.",
+  //       "Add juice of one lemon, and ¼ teaspoon red pepper flakes. Garnish with 2 tablespoons chopped parsley and toss with pasta if desired. Serve immediately .",
+  //   ],
+  //   link: "https://therecipecritic.com/brown-butter-spicy-garlic-shrimp/",
+  //   slug: "spicy-garlic-shrimp"
+  // },
   {
     id: 2,
     title: "Dill Pickles",
